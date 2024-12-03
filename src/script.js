459968e390d9b5b9666d43c8742675f27dca4317
Santiago Gomez <Santigomez1129@gmail.com>
2024-12-03 18:06:44 -0300

    The canvas and it's background image resizes to it's container. Is it possible to draw on the canvas

--- src/script.js
+++ src/script.js
@@ -1,7 +1,14 @@
 const players = document.getElementsByClassName('player');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 // const playersContainer = document.getElementsByClassName('players-container')
 
 const court = document.getElementById('court')
+
+court.addEventListener('click', () => {
+    console.log(court.offsetHeight, court.offsetWidth)
+})
 
 for (let i = 0; i < players.length; i++) {
 
@@ -25,11 +32,11 @@ court.addEventListener('drop', e => {
     const draggedId = e.dataTransfer.getData("text");
     const draggedElement = document.getElementById(draggedId);
 
-    // Calculate the position of the drop within the big div
+
     const x = e.clientX - court.offsetLeft - draggedElement.offsetWidth / 2;
     const y = e.clientY - court.offsetTop - draggedElement.offsetHeight / 2;
 
-    // Prevent divs from being placed outside the big div
+
     const newX = Math.max(0, Math.min(x, court.offsetWidth - draggedElement.offsetWidth));
     const newY = Math.max(0, Math.min(y, court.offsetHeight - draggedElement.offsetHeight));
 
@@ -40,3 +47,67 @@ court.addEventListener('drop', e => {
     court.appendChild(draggedElement);
 
 })
+
+canvas.addEventListener('mousedown', startDrawing)
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup', stopDrawing)
+canvas.addEventListener('mouseleave', stopDrawing)
+window.addEventListener('load', resizeCanvas);
+window.addEventListener('resize', resizeCanvas)
+
+let isDrawing = false
+let startX, startY
+let lastX = 0
+let lastY = 0
+
+function resizeCanvas() {
+    canvas.width = court.offsetWidth - 35;
+    canvas.height = court.offsetHeight - 35;
+
+}
+
+function startDrawing(event) {
+    isDrawing = true
+
+    const rect = canvas.getBoundingClientRect();
+    startX = event.clientX - rect.left;
+    startY = event.clientY - rect.top;
+
+
+    lastX = startX;
+    lastY = startY;
+
+
+}
+
+function draw(event) {
+    if (!isDrawing) return
+
+    const rect = canvas.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+
+    ctx.lineWidth = 3
+
+
+    ctx.beginPath()
+
+
+    ctx.moveTo(lastX, lastY)
+
+
+    ctx.lineTo(offsetX, offsetY)
+
+    ctx.stroke();
+
+    lastX = offsetX;
+    lastY = offsetY;
+
+
+}
+
+
+function stopDrawing(event) {
+    isDrawing = false
+}
+
